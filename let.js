368e8bc3f1c2ed009815e87fe60914d93bb43fc6
DevikaSomagani <124118193+DevikaSomagani@users.noreply.github.com>
2023-02-01 12:09:09 +0530

    Add files via upload

---
+++ let.js
@@ -0,0 +1,6 @@
+var x=5;
+if(true){
+    let x=3;
+    console.log(x);
+}
+console.log(x)
---
+++ prog1.js
@@ -0,0 +1,6 @@
+const pi=3.14
+var x=3
+for(let i=0;i<x;i++)
+console.log(i);
+console.log(pi);
+console.log(x);
